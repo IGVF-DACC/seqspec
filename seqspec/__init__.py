495bd142e0993b0d69b62cf14f9bf91fdd96c9b6
Sina Booeshaghi <sbooeshaghi@gmail.com>
2025-07-20 12:40:42 -0700

    - fixed bug in read get file by id - updated seqspec index to initialize pydantic models with named args - removed tox, changed to pytest as a test manager - cleaned up internal api for seqspec onlist (todo, add subcommands list, download, join)

--- seqspec/__init__.py
+++ seqspec/__init__.py
@@ -16,8 +16,8 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.3.2.dev20+gf85f4b8.d20250630"
-__version_tuple__ = version_tuple = (0, 3, 2, "dev20", "gf85f4b8.d20250630")
+__version__ = version = "0.3.2.dev23+g8a0b1e2.d20250717"
+__version_tuple__ = version_tuple = (0, 3, 2, "dev23", "g8a0b1e2.d20250717")
 
 
 def get_version():
