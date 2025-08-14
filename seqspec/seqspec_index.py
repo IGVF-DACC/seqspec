6b8ef5b2561edfecefefe99038053cb7969d6233
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2025-08-14 13:18:53 -0500

    add region_type: sgrna_target (#72)
    * added sgrna_target as region_type
    
    * fixed format_kallisto_bus_force_single

--- seqspec/seqspec_index.py
+++ seqspec/seqspec_index.py
@@ -319,25 +319,21 @@ def get_index_by_primer(
     return {read_id: new_rcs, "strand": rdc.read.strand}
 
 
+FEATURE_REGION_TYPES = {"CDNA", "GDNA", "PROTEIN", "TAG", "SGRNA_TARGET"}
+
+
 def format_kallisto_bus(indices, subregion_type=None):
     bcs = []
     umi = []
     feature = []
-    for idx, region in enumerate(indices):
-        rg_strand = region.pop("strand")  # noqa
-        for rgn, cuts in region.items():
-            for cut in cuts:
-                if cut.region_type.upper() == "BARCODE":
-                    bcs.append(f"{idx},{cut.start},{cut.stop}")
-                elif cut.region_type.upper() == "UMI":
-                    umi.append(f"{idx},{cut.start},{cut.stop}")
-                elif (
-                    cut.region_type.upper() == "CDNA"
-                    or cut.region_type.upper() == "GDNA"
-                    or cut.region_type.upper() == "PROTEIN"
-                    or cut.region_type.upper() == "TAG"
-                ):
-                    feature.append(f"{idx},{cut.start},{cut.stop}")
+    for idx, obj in enumerate(indices):
+        for cut in obj.rcv:
+            if cut.region_type.upper() == "BARCODE":
+                bcs.append(f"{idx},{cut.start},{cut.stop}")
+            elif cut.region_type.upper() == "UMI":
+                umi.append(f"{idx},{cut.start},{cut.stop}")
+            elif cut.region_type.upper() in FEATURE_REGION_TYPES:
+                feature.append(f"{idx},{cut.start},{cut.stop}")
     if len(umi) == 0:
         umi.append("-1,-1,-1")
     if len(bcs) == 0:
@@ -354,24 +350,17 @@ def format_kallisto_bus_force_single(indices, subregion_type=None):
     longest_feature = None
     max_length = 0
 
-    for idx, region in enumerate(indices):
-        rg_strand = region.pop("strand")  # noqa
-        for rgn, cuts in region.items():
-            for cut in cuts:
-                if cut.region_type.upper() == "BARCODE":
-                    bcs.append(f"{idx},{cut.start},{cut.stop}")
-                elif cut.region_type.upper() == "UMI":
-                    umi.append(f"{idx},{cut.start},{cut.stop}")
-                elif (
-                    cut.region_type.upper() == "CDNA"
-                    or cut.region_type.upper() == "GDNA"
-                    or cut.region_type.upper() == "PROTEIN"
-                    or cut.region_type.upper() == "TAG"
-                ):
-                    length = cut.stop - cut.start
-                    if length > max_length:
-                        max_length = length
-                        longest_feature = f"{idx},{cut.start},{cut.stop}"
+    for idx, coord in enumerate(indices):
+        for cut in coord.rcv:
+            if cut.region_type.upper() == "BARCODE":
+                bcs.append(f"{idx},{cut.start},{cut.stop}")
+            elif cut.region_type.upper() == "UMI":
+                umi.append(f"{idx},{cut.start},{cut.stop}")
+            elif cut.region_type.upper() in FEATURE_REGION_TYPES:
+                length = cut.stop - cut.start
+                if length > max_length:
+                    max_length = length
+                    longest_feature = f"{idx},{cut.start},{cut.stop}"
 
     if len(umi) == 0:
         umi.append("-1,-1,-1")
